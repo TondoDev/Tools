c3815a1437ed99846de50c303343bde017308b9d
TondoDev <tondodeveloper@gmail.com>
2016-01-25 21:59:12 +0100

    [certimport] added tests for adding root certificate
    Some interesting things were discovered.
    1. Sharing SSL context is automatic durring life of ssl context.
    2. adding entry with already existing alias, don't have to necessarily
    crash. When entry points to trusted certificate it is overriden.

--- certimport/src/test/java/org/tondo/certimport/TrustConnectionTest.java
+++ certimport/src/test/java/org/tondo/certimport/TrustConnectionTest.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 
+import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLException;
 import javax.net.ssl.SSLHandshakeException;
 
@@ -87,11 +88,70 @@ public class TrustConnectionTest extends StandardTestBase{
 		// now we add root certificate to connection manager
 		CertStoreResult result = emptyManager.addRootCertificate(fburl, "facebook");
 		assertEquals("One certificate should be added", 1, result.getCertificatesAdded());
+		assertNull("Cetificat was untrusted before", result.getMatchingCertificate());
 		
 		// now connection should be established
 		fbConn = emptyManager.getConnection(fburl);
 		checkReadabilityFromConnection(fbConn);
+		
+		// TODO this won't work because  connecting to same site multiple times shares SSL session
+		// need to be solved somehow
+		// once again try add same certificate
+//		result = emptyManager.addRootCertificate(fburl, "fb2");
+//		// because default behavior is add even if exist
+//		assertEquals("One certificate should be added even if exists", 1, result.getCertificatesAdded());
+//		assertNotNull("Match in trustore was found", result.getMatchingCertificate());
+//		assertEquals("Matched shoudl be root certificate", result.getMatchingCertificate(), result.getServerCertChain()[0]);
 	}
 	
-
+	@Test
+	public void testAddingOneCertMultipleTimes() throws FileNotFoundException, IOException {
+		TrustedConnectionManager emptyManager = null;
+		try (InputStream input = new FileInputStream(this.inResourceDir("trustempty").toString())) {
+			emptyManager = new TrustedConnectionManager(input, "trusted".toCharArray());
+		}
+		
+		URL fburl = new URL("https://www.facebook.com");
+		// now we add root certificate to connection manager
+		CertStoreResult result = emptyManager.addRootCertificate(fburl, "facebook");
+		assertEquals("One certificate should be added", 1, result.getCertificatesAdded());
+		assertNull("Cetificat was untrusted before", result.getMatchingCertificate());
+		
+		// once again try add same certificate
+		result = emptyManager.addRootCertificate(fburl, "fb2");
+		// handshake process occurs because ssl context is reloaded so session sharing is not applied here
+		
+		// because default behavior is doesn't add if exist
+		assertEquals("One certificate should be added even if exists", 0, result.getCertificatesAdded());
+		assertNotNull("Match in trustore was found", result.getMatchingCertificate());
+		assertEquals("Matched shoudl be root certificate", result.getMatchingCertificate(), 
+				result.getServerCertChain()[result.getServerCertChain().length - 1]);
+	}
+	
+	
+	@Test
+	public void testAddingCertWithExistingAlias() throws FileNotFoundException, IOException {
+		TrustedConnectionManager emptyManager = null;
+		try (InputStream input = new FileInputStream(this.inResourceDir("trustempty").toString())) {
+			emptyManager = new TrustedConnectionManager(input, "trusted".toCharArray());
+		}
+		
+		URL fburl = new URL("https://www.facebook.com");
+		// now we add root certificate to connection manager
+		CertStoreResult result = emptyManager.addRootCertificate(fburl, "facebook");
+		assertEquals("One certificate should be added", 1, result.getCertificatesAdded());
+		assertNull("Cetificat was untrusted before", result.getMatchingCertificate());
+		
+		URL oraUrl = new URL("https://docs.oracle.com/en/");
+		// lets add certificate with already used alias
+		result = emptyManager.addRootCertificate(oraUrl, "facebook");
+		
+		// because alias points to trusted certificate, it is overridden so connection to previous one 
+		// should fail
+		try {
+			HttpsURLConnection fbConn = emptyManager.getConnection(fburl);
+			checkReadabilityFromConnection(fbConn);
+			fail("SSLHandshakeException expected, because alias entry was overridden");
+		} catch (SSLHandshakeException e) {}
+	}
 }
