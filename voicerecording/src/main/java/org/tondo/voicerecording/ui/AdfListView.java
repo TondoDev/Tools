869436d0f68aa36d77beb6bf11f054d475134d85
TondoDev <tondodeveloper@gmail.com>
2019-02-03 16:48:18 +0100

    [rec] drag and drop WIP

--- voicerecording/src/main/java/org/tondo/voicerecording/ui/AdfListView.java
+++ voicerecording/src/main/java/org/tondo/voicerecording/ui/AdfListView.java
@@ -4,6 +4,9 @@ import org.tondo.voicerecording.adf.AdfEntry;
 
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.util.Callback;
 
 /**
@@ -14,6 +17,35 @@ import javafx.util.Callback;
 public class AdfListView extends ListView<AdfEntry> {
 	
 	private class AdfCell extends ListCell<AdfEntry> {
+		
+		public AdfCell() {
+			setOnDragDetected(e -> {
+				System.out.println("Drag detected!");
+				Dragboard db = this.startDragAndDrop(TransferMode.MOVE);
+				ClipboardContent content = new ClipboardContent();
+				content.putString(this.getText());
+				db.setContent(content);
+				e.consume();
+			});
+			
+			
+			setOnDragOver(e -> {
+				if  (e.getGestureSource() != this) {
+					e.acceptTransferModes(TransferMode.MOVE);
+					//System.out.println(e.getDragboard().getString());
+				}
+				e.consume();
+			});
+			
+			
+			setOnDragEntered(e -> {
+				System.out.println("Enter " + this.getText());
+			});
+			
+			setOnDragExited(e -> {
+				System.out.println("exit " + this.getText());
+			});
+		}
 		@Override
 		protected void updateItem(AdfEntry item, boolean empty) {
 			super.updateItem(item, empty);
